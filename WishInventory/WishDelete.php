c2fab33cb87f1748ea252a8d65a5bd7e155597fa
NEGA <negativegrounds@gmail.com>
2021-04-20 17:02:31 -0400

    troubleshooting

--- WishInventory/WishDelete.php
+++ WishInventory/WishDelete.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <div id="nav">
+        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+        <div id="nav-placeholder"></div>
+        <script>
+            $(function(){
+            $("#nav-placeholder").load("Wishnavbar.html");
+            });
+        </script>
+    </div>
+  <title>Delete Item</title>
+</head>
+<body>
 <?php
 // include_once("../db_connect.php");
 include 'WishLookup.php';
@@ -14,3 +32,5 @@ echo "$customer_ID";
   
 // $conn->close();
 ?>
+</body>
+</html>
