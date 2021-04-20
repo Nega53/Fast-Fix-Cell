b0c50e2f2c52aac290c02423c1af3e398202e950
NEGA <negativegrounds@gmail.com>
2021-04-20 16:46:24 -0400

    file include in wish

--- WishInventory/WishDelete.php
+++ WishInventory/WishDelete.php
@@ -1,10 +1,12 @@
 <?php
 include_once("../db_connect.php");
+include_once("WishLookup.php");
 
 $sqld = "DELETE FROM ffc_wish_inventory WHERE customer_ID = '".$customer_ID."'";
 
 if ($conn->query($sqld) === TRUE) {
     echo "Record deleted successfully";
+    echo '<p><a href="../index.php">Home</a></p>';
   } else {
     echo "Error deleting record: " . $conn->error;
   }
