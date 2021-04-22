2d641223bcc9d8f73bda22b1384a3c9501fea37b
NEGA <negativegrounds@gmail.com>
2021-04-22 13:35:33 -0400

    Testing globals

--- WishInventory/WishLookup.php
+++ WishInventory/WishLookup.php
@@ -22,7 +22,6 @@ include_once("../db_connect.php");
 
 //Form Input
 $customer_ID = $conn->real_escape_string($_REQUEST['customer_ID']);
-$GLOBALS[$customer_ID] = $customer_ID; 
 
 //Attempt to fetch data from table
 $sql = "SELECT * FROM ffc_wish_inventory WHERE customer_ID = '".$customer_ID."'";
