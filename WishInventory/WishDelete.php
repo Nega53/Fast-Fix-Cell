2aa6bbfce3b28e4085735181aeac3325c07b046d
NEGA <negativegrounds@gmail.com>
2021-04-22 14:09:21 -0400

    Testing Delete function

--- WishInventory/WishDelete.php
+++ WishInventory/WishDelete.php
@@ -1,17 +1,15 @@
 <?php
-// include_once("../db_connect.php");
-//Edit variable since its not being passed
+include_once("../db_connect.php");
 $customer_ID = $_REQUEST['customer_ID'];
-echo "<p>". $customer_ID ."</p>";
 
-// $sqld = "DELETE FROM ffc_wish_inventory WHERE customer_ID = '".$customer_ID."'";
+$sqld = "DELETE FROM ffc_wish_inventory WHERE customer_ID = '".$customer_ID."'";
 
-// if ($conn->query($sqld) === TRUE) {
-//     echo "Record deleted successfully";
-//     echo '<p><a href="../index.php">Home</a></p>';
-//   } else {
-//     echo "Error deleting record: " . $conn->error;
-//   }
+if ($conn->query($sqld) === TRUE) {
+    echo "Record deleted successfully";
+    echo '<p><a href="../index.php">Home</a></p>';
+  } else {
+    echo "Error deleting record: " . $conn->error;
+  }
   
-// $conn->close();
+$conn->close();
 ?>
