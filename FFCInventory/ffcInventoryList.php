b839c6582782503f6c4d173303ad20adeecff7f1
NEGA <negativegrounds@gmail.com>
2021-04-29 15:06:04 -0400

    Code Debug

--- FFCInventory/ffcInventoryList.php
+++ FFCInventory/ffcInventoryList.php
@@ -105,22 +105,7 @@ $conn->close();
             <?php while($items = mysqli_fetch_assoc($result)){?>
                 <?php
                     if($items['qr_code'] === null || empty($items['qr_code'])){
-                        // //Create json and insert into table
-                        $jsonobj = array("qrcode"=>$items['item_ID'], "name"=>$items['item_name'], "brand"=>$items['brand'],
-                        "model"=>$items['phone_model'], "type"=>$items['accessory_type'], "quantity"=>$items['item_quantity']);
-
-                        $text = json_encode($jsonobj, JSON_FORCE_OBJECT);
-                        echo "<script>console.log($text);</script>";
-
-                        $sqlI = "UPDATE ffc_inventory SET qr_code = '".$text."' WHERE item_ID = '".$items['item_ID']."'";
-                        
-                        if($conn->query($sqlI) === true){
-                            echo "<script>console.log('Object Stored');</script>";
-                        } else {
-                            echo "Unable to execute $sql." . $conn->error;
-                        }
-
-                        $conn->close();
+                        include 'ffcUpdate.php';
                     }
                 ?>
                 <tr id="<?php echo $items['item_ID']; ?>">
