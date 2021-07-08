346d5eb75a5efff6d332558c37178319767278d3
NEGA <negativegrounds@gmail.com>
2021-07-08 00:42:31 -0400

    Testing pdb

--- PhoneDatabase/phoneBrands.php
+++ PhoneDatabase/phoneBrands.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <?php include_once("../db_connect.php"); ?>
     <div id="nav">
         <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
         <div id="nav-placeholder"></div>
