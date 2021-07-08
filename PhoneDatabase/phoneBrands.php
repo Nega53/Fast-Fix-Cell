5d38ca7a9432fa20f41d631af15f6b5f2c96ac43
NEGA <negativegrounds@gmail.com>
2021-07-08 00:36:16 -0400

    Added Main Phone Database

---
+++ PhoneDatabase/phoneBrands.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <div id="nav">
+        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+        <div id="nav-placeholder"></div>
+        <script>
+            $(function(){
+            $("#nav-placeholder").load("phonedbnavbar.html");
+            });
+        </script>
+    </div>
+    <title>Phone Brand Listing</title>
+</head>
+<body>
+<?php
+    $sql = "SELECT DISTINCT Brand, COUNT(Brand) AS Brand_Total FROM mobile_phones_database_by_teoalida";
+    $result = $conn->query($sql);
+    $conn->close();
+?>
+    <?php while($pitems = mysqli_fetch_assoc($result)){ ?>
+        <d1>
+            <dt><?php echo $pitems['Brand']; ?></dt>
+            <dd><?php echo $pitems['Brand_Total']; ?></dd>
+        </d1>
+    <?php } ?>
+</body>
+</html>
